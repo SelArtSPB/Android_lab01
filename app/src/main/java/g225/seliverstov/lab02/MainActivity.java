47a60ddd84f8fa31c047cb12d75c8467a5b469c5
g225-seliverstov <sel_art_spb@mail.com>
2024-02-15 12:46:48 +0300

    last version

--- app/src/main/java/g225/seliverstov/lab02/MainActivity.java
+++ app/src/main/java/g225/seliverstov/lab02/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         EditText txt_B = findViewById(R.id.TextBoxB);
         TextView txt_Res = findViewById(R.id.TextViewResult);
 
+
+
         Button ADD = findViewById(R.id.buttonADD);
         Button SUB = findViewById(R.id.buttonSUB);
         Button MULT = findViewById(R.id.buttonMULT);
@@ -34,23 +37,22 @@ public class MainActivity extends AppCompatActivity {
         Button SQRT = findViewById(R.id.buttonSQRT);
         Button POW = findViewById(R.id.buttonPOW);
 
+        RadioButton Radian = findViewById(R.id.Radians);
+        RadioButton Numbers = findViewById(R.id.Numbers);
+
         String Action_A = txt_A.getText().toString();
         String Action_B = txt_B.getText().toString();
 
 
-        float a = 0,  r = 0 , b = 0;
-
+        double a = 0,  r = 0 , b = 0;
             try {
-                a = Float.parseFloat(Action_A);
-                b = Float.parseFloat(Action_B);
+                a = Double.parseDouble(Action_A);
+                b = Double.parseDouble(Action_B);
+            } catch (Exception e1) {
+                txt_Res.setText("input blocks are empty or incorrect number format is entered");
+                return;
             }
-            catch (Exception e1)
-            {
-               txt_Res.setText("input blocks are empty or incorrect number format is entered");
-               return;
-            }
-        if ( a != 0 && b != 0)
-            {
+
                 if (v == ADD) r = a + b;
                 if (v == SUB) r = a - b;
                 if (v == MULT) r = a * b;
@@ -63,18 +65,27 @@ public class MainActivity extends AppCompatActivity {
                     }
                     r = a / b;
                 }
-                if (v == POW) r = (float) Math.pow(a, b);
-                if (v == SIN) r = (float) Math.sin(a);
-                if (v == COS) r = (float) Math.cos(a);
-                if (v == TAN) r = (float) Math.tan(a);
+                if (v == POW) r =  Math.pow(a, b);
+                if (v == SIN) r = Math.sin(a);
+                if (v == COS) r = Math.cos(a);
+                if (v == TAN){
+                    if (a == 90 || a == 270){
+                        txt_Res.setText("Error 90 or 270 tan");
+                        return;
+                    }
+                    else {
+                        r = Math.tan(a);
+                    }
+                }
+
                 if (v == SQRT) {
                     if (a < 0) {
                         txt_Res.setText("You can't take the sqrt of a negative number");
                         return;
                     }
-                    r = (float) Math.sqrt(a);
+                    r = Math.sqrt(a);
                 }
-            }
+
         String recordResult = String.valueOf(r);
         txt_Res.setText(recordResult);
     }
